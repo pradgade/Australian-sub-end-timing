882b55197d6a14e0cfc8853558ceab481bf0392f
pradgade <pradnya.gade@twinkl.co.uk>
2023-08-09 21:52:23 +0100

    Added query for renewals

--- austrailian subscriptions.sql
+++ austrailian subscriptions.sql
@@ -18,9 +18,11 @@ WHERE ux_end_date BETWEEN DATE_SUB(CURDATE(), INTERVAL 2 WEEK) AND CURDATE() -- 
 	AND uis.country_id = 12 -- For Australia
 ;
 
-SELECT MIN(ux_end_date),
-			 MAX(ux_end_date)
-FROM aus;
+SELECT ux_end_date,
+			 COUNT(DISTINCT user_id)
+FROM aus
+GROUP BY aus.ux_end_date;
+
 
 -- Extracted users who viewed resources 12 hours leading up to the end of the sub end date
 DROP TEMPORARY TABLE IF EXISTS res_viewed;
@@ -49,11 +51,11 @@ CREATE TEMPORARY TABLE rv_dup AS
 SELECT *
 FROM res_viewed;
 
--- Created temporary table for analysis
+-- Created temporary table for viewed_analysis
 
-DROP TEMPORARY TABLE IF EXISTS analysis;
+DROP TEMPORARY TABLE IF EXISTS viewed_analysis;
 
-CREATE TEMPORARY TABLE analysis AS
+CREATE TEMPORARY TABLE viewed_analysis AS
 SELECT t1.ux_end_date, t1.no_views, t2.number_of_users_viewed
 FROM (
 			 SELECT aus.ux_end_date,
@@ -75,26 +77,46 @@ FROM (
 
 
 SELECT *
-FROM analysis;
+FROM viewed_analysis;
 
--- Most popular career_id among these australian users
-SELECT career_id,
-			 COUNT(career_id) AS count
-FROM res_viewed
-GROUP BY career_id
-ORDER BY count DESC
-LIMIT 5;
+-- Out of users who had resource views in the window how many renewed?
 
--- In general popular career_id among these australian users during the last two weeks
+SELECT ux_end_date,
+			 COUNT(DISTINCT user_id)
+FROM res_viewed AS rv
+	JOIN sub_ux_ind_extra AS suie
+		ON rv.sub_ux_id = suie.renewal_of
+GROUP BY ux_end_date;
 
-SELECT career_id,
-			 COUNT(career_id) AS count
-FROM analytics.meta_temp_resource_views
-WHERE datetime BETWEEN DATE_SUB(CURDATE(), INTERVAL 2 WEEK) AND CURDATE()
-	AND country_id = 12
-GROUP BY career_id
-ORDER BY count DESC
-LIMIT 5;
+-- Out of users who didn't view in the window how many renewed?
+
+SELECT aus.ux_end_date,
+			 COUNT(DISTINCT aus.user_id)
+FROM aus
+	LEFT JOIN rv_dup AS rv
+		ON aus.user_id = rv.user_id
+	JOIN sub_ux_ind_extra AS suie
+		ON aus.sub_ux_id = suie.renewal_of
+WHERE rv.user_id IS NULL
+GROUP BY aus.ux_end_date;
+
+
+SELECT DISTINCT suie.renewal_type
+FROM aus
+	LEFT JOIN rv_dup AS rv
+		ON aus.user_id = rv.user_id
+	JOIN sub_ux_ind_extra AS suie
+		ON aus.sub_ux_id = suie.renewal_of
+WHERE rv.user_id IS NULL;
+
+-- Users who were cancelled
+
+SELECT rv.ux_end_date,
+			 COUNT(DISTINCT rv.user_id)
+FROM res_viewed AS rv
+	JOIN cancellation_downgrade_pause_metrics AS cdpm
+		ON rv.sub_ux_id = cdpm.sub_ux_id
+GROUP BY ux_end_date;
 
 
 -- Extracted users who downloaded resources 12 hours leading up to the end of the sub end date
@@ -106,7 +128,8 @@ SELECT a.sub_ux_id,
 			 a.sub_type,
 			 a.ux_end_date,
 			 gmtdl.datetime,
-			 gmtdl.career_id
+			 gmtdl.career_id,
+			 gmtdl.country_id
 FROM aus AS a
 	JOIN george_meta_temp_download_log AS gmtdl
 		ON a.user_id = gmtdl.user_id
@@ -114,30 +137,60 @@ WHERE DATE(gmtdl.datetime) = a.ux_end_date
 	AND TIME(gmtdl.datetime) BETWEEN '12:00:00' AND '23:59:59';
 
 
--- Most popular career_id among these australian users
-SELECT career_id,
-			 COUNT(career_id)
-FROM res_down
-GROUP BY career_id;
-
-
--- Rough work
-
 SELECT *
+FROM res_down;
+
+-- Created duplicate of temporary table res_viewed to access twice in the below analysis table
+
+DROP TEMPORARY TABLE IF EXISTS rd_dup;
+
+CREATE TEMPORARY TABLE rd_dup AS
+SELECT *
+FROM res_down;
+
+-- Created temporary table for download_analysis
+
+DROP TEMPORARY TABLE IF EXISTS download_analysis;
+
+CREATE TEMPORARY TABLE download_analysis AS
+SELECT t1.ux_end_date, t1.no_downloads, t2.number_of_users_downloaded
+FROM (
+			 SELECT aus.ux_end_date,
+							COUNT(DISTINCT aus.user_id) AS no_downloads
+			 FROM aus
+				 LEFT JOIN rd_dup AS rd
+					 ON aus.user_id = rd.user_id
+			 WHERE rd.user_id IS NULL
+			 GROUP BY aus.ux_end_date
+		 ) t1
+	JOIN
+(
+	SELECT ux_end_date,
+				 COUNT(DISTINCT user_id) AS number_of_users_downloaded
+	FROM res_down
+	GROUP BY ux_end_date
+) t2
+		ON t1.ux_end_date = t2.ux_end_date;
+
+
+SELECT aus.ux_end_date,
+			 COUNT(DISTINCT aus.user_id) AS no_downloads
 FROM aus
-	JOIN analytics.meta_temp_resource_views AS mtrv
-		ON aus.user_id = mtrv.user_id
-WHERE date BETWEEN DATE_SUB(CURDATE(), INTERVAL 2 WEEK) AND CURDATE();
+	LEFT JOIN rd_dup AS rd
+		ON aus.user_id = rd.user_id
+WHERE rd.user_id IS NULL
+GROUP BY aus.ux_end_date;
 
-SELECT DISTINCT (outcome_type)
-FROM cancellation_downgrade_pause_metrics;
+
+
+SELECT ux_end_date,
+			 COUNT(DISTINCT user_id) AS number_of_users_downloaded
+FROM res_down
+GROUP BY ux_end_date;
 
 SELECT *
-FROM aus AS a
-	JOIN analytics.meta_temp_resource_views AS mtrv
-		ON a.user_id = mtrv.user_id
-		AND DATE_FORMAT(mtrv.datetime, '%Y-%m-%d') = a.ux_end_date
-WHERE mtrv.datetime BETWEEN CONCAT(a.ux_end_date, ' 23:59:00') AND DATE_SUB(CONCAT(a.ux_end_date, ' 23:59:00'), INTERVAL 12 HOUR);
+FROM download_analysis;
 
 
-
+SELECT DISTINCT outcome_type
+FROM cancellation_downgrade_pause_metrics;
